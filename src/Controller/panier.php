aaeda19f6f65b8cebeb60ad2feb6555261950940
Alexandre <Alexandre.chekroun@etu.univ-paris1.fr>
2021-06-23 09:16:35 +0200

    ajout redis premiere etape

--- src/Controller/panier.php
+++ src/Controller/panier.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use App\Repository\panierRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -24,6 +25,36 @@ class panier extends AbstractController
     {
 
         return $this->render('panier.html.twig');
+    }
+
+    /**
+     * @return Response
+     * @Route("/ajouter/{id}", name="panier_ajouter", requirements={"id"="\d+"})
+     */
+    public function ajouter(int $id): Response
+    {
+        $panierRepo = new panierRepository();
+
+        $panierRepo->ajouterItem($id);
+
+        return $this->render('panier.html.twig', [
+            "panier" => $panierRepo->getPanier()
+        ]);
+    }
+
+    /**
+     * @return Response
+     * @Route("/retirer/{id}", name="panier_retirer", requirements={"id"="\d+"})
+     */
+    public function retirer(int $id): Response
+    {
+        $panierRepo = new panierRepository();
+
+        $panierRepo->ajouterItem($id);
+
+        return $this->render('panier.html.twig', [
+            "panier" => $panierRepo->getPanier()
+        ]);
     }
 
 }
--- src/Repository/panierRepository.php
+++ src/Repository/panierRepository.php
@@ -3,8 +3,70 @@
 
 namespace App\Repository;
 
+use predis;
 
 class panierRepository
 {
+    public function getRedis(){
+
+        Predis\Autoloader::register();
+
+        return new Predis\Client(
+            array(
+                "scheme" => "tcp",
+                "host" => "localhost",
+                "port" => 6379
+            )
+        );
+    }
+
+    public function ajouterItem(int $id){
+        $redis = $this->getRedis();
+
+        $ttl_5mn = 60 * 5; // 5mn expiration
+
+        if($redis->exists($id)){
+            $redis->incr($id);
+            // On prolonge la durée de vie du panier à chaque interaction
+            $redis->expire('panier', $ttl_5mn);
+        } else {
+            $redis->sadd('panier', (array) $id);
+            $redis->expire('panier', $ttl_5mn);
+            $redis->incr($id);
+            $redis->expire($id, $ttl_5mn);
+        }
+    }
+
+    public function retirerItem(int $id){
+        $redis = $this->getRedis();
+
+        $ttl_5mn = 60 * 5; // 5mn expiration
+
+        if($redis->exists($id)){
+            $redis->del((array) $id);
+            $redis->srem('panier', $id);
+            // On prolonge la durée de vie du panier à chaque interaction
+            $redis->expire('panier', $ttl_5mn);
+        }
+    }
+
+    public function getPanier(){
+        $redis = $this->getRedis();
+
+        $output = [];
+
+        if($redis->exists('panier')){
+            $panier = $redis->smembers('panier');
+            foreach ($panier as $key){
+                $output[$key] = $redis->get($key);
+            }
+        }
+        return $output;
+    }
+
+    public function viderPanier(){
+        $redis = $this->getRedis();
+
+    }
 
 }
