423b977641def198f436944de6f371d95bb9feeb
Alexandre <Alexandre.chekroun@etu.univ-paris1.fr>
2021-07-07 10:27:04 +0200

    fix elastic search 1/2

--- src/Controller/RechercheController.php
+++ src/Controller/RechercheController.php
@@ -1,43 +1,59 @@
 <?php
+
 namespace App\Controller;
+
+use App\Repository\articleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\rechercheRepository;
+use App\Repository\RechercheRepository;
 
 class RechercheController extends AbstractController
 {
     /**
-     * @Route("/recherche", name="rechercheFiltre")
+     * @Route("/rechercheFiltre", name="rechercheFiltre")
      * @param Request $request
      * @return Response
      */
     public function rechercheFiltre(Request $request): Response
     {
-        $livres = new rechercheRepository();
+        $livres = new RechercheRepository();
         $test1 = $livres->searchFilter('auteur', 'e');
         $resultSearchFilter = json_decode($test1);
         $res = $resultSearchFilter->{'hits'}->{'hits'};
         //print_r($res[0]->{'_source'});
-        return $this->render('recherche.html.twig', [
+        return $this->render('home/recherche.html.twig', [
             "livres" => $res
         ]);
     }
     /**
+     * @Route("/rechercheAll", name="rechercheAll")
+     * @param Request $request
      * @return Response
-     * @Route("/recherche", name="rechercheAll")
      */
-    public function rechercheAll(): Response
+    public function rechercheAll(Request $request): Response
     {
-        $livres = new rechercheRepository();
-        $test = $livres->searchInAll('xav');
-        $resultSearchFilter = json_decode($test);
-        $res = $resultSearchFilter->{'hits'}->{'hits'};
-        print_r($res[0]->{'_source'});
-        return $this->render('catalogue.html.twig', [
-            "livres" => $livres->getAllItem()
-        ]);
+        if($request->isMethod('post')){
+            $posts = $request->request->all();
+            $post = $request->request->get("searchCatalogue");
+            $recherche = new RechercheRepository();
+            $result= $recherche->searchInAll($post);
+            $resultSearchFilter = json_decode($result);
+            $res = $resultSearchFilter->{'hits'}->{'hits'};
+            $livres = new articleRepository();
+            // print_r($res); //$res[0]->{'_source'}
+            $arrayTest = [];
+            for( $i= 0 ; $i < count($res) ; $i++ )
+            {
+                $livre = $livres->getItemById($res[$i]->{'_source'}->{'id'});
+                array_push($arrayTest, $livre);
+            }
+            return $this->render('home/recherche.html.twig', [
+                "listLivre" => $arrayTest
+            ]);
+        }
+        return $this->render('home/index.html.twig');
     }
 }
